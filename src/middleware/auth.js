727f8f4203c98d0fc4c2d90d96494e51ed130d14
Diego Cruz <dacruzt@outlook.com>
2021-06-23 14:39:24 -0500

    middleware auth with token

---
+++ src/middleware/auth.js
@@ -0,0 +1,41 @@
+const jwt = require("jsonwebtoken");
+const boom = require("@hapi/boom");
+const validateFacebookToken = require("../utils/validateFacebookToken");
+const validateGoogle = require("../utils/validateGoogleToken");
+
+/**
+ * Verifies if a token is valid
+ */
+async function middleware(req, res, next) {
+  const bearerToken = req.headers["authorization"];
+  if (!bearerToken) {
+    return next(boom.unauthorized("Token required"));
+  }
+  const [bearer, token] = bearerToken.split(" ");
+  if (bearer !== "Bearer") {
+    return next(boom.unauthorized("Invalid token format"));
+  }
+
+  try {
+    const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+    if (decoded) {
+      return next();
+    }
+  } catch (error) {}
+
+  //verify if is google oauth token
+  const isGoogleToken = await validateGoogle(token);
+  if (isGoogleToken) {
+    return next();
+  }
+
+  //verify if is facebook oauth token
+  const isFacebookToken = validateFacebookToken(token);
+  if (isFacebookToken) {
+    return next();
+  }
+  //is not valid
+  next(boom.unauthorized("Invalid token"));
+}
+
+module.exports = middleware;
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const authController = require("../controllers/auth");
+const auth = require("../middleware/auth");
 
 const router = express.Router();
 
-router.post("/register", authController.register);
+router.post("/register", auth, authController.register);
 
 router.post("/login", authController.login);
 
