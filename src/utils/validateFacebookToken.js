94764b36d688c56f66c6ba77b0158b61e1c013b6
Diego Cruz <dacruzt@outlook.com>
2021-07-02 16:56:49 -0500

    facebook prueba

--- src/utils/validateFacebookToken.js
+++ src/utils/validateFacebookToken.js
@@ -1,6 +1,7 @@
 const https = require("https");
 const userId = process.env.CLIENT_ID;
 module.exports = function (token) {
+  console.log(`https://graph.facebook.com/${userId}?input_token=${token}`);
   https.get(
     `https://graph.facebook.com/${userId}?input_token=${token}`,
     (res) => {
